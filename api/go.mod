e0c301c63421f8917cee25320e2a6955452b0a5d
Petter Daae <petter.daae@gmail.com>
2020-08-15 13:04:28 +0200

    Connect api with database

---
+++ api/go.mod
@@ -0,0 +1,8 @@
+module inf319
+
+go 1.14
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.8.0
+)
--- api/main.go
+++ api/main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 )
 
 func welcome(w http.ResponseWriter, r *http.Request) {
-	log.Println("Welcome called")
-	fmt.Fprintf(w, "Welcome!")
+	rows, err := QueryDb("SELECT * FROM MyFirstTable")
+	if err != nil {
+		log.Fatalf("welcome query failed: %v", err)
+		return
+	}
+
+	for rows.Next() {
+		var theBestColumn string
+		rows.Scan(&theBestColumn)
+		log.Println(theBestColumn)
+	}
 }
 
 func main() {
-	log.Println("Listening for http calls ...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load environment variables from .env file, exiting ...")
+		return
+	}
+
+	port := os.Getenv("PORT")
+
+	log.Println("Listening for REST calls on port " + port)
+
 	http.HandleFunc("/", welcome)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":" + port, nil))
 }
