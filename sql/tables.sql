2d7b140318f20d4a8f57e9d4dd41ca58347b4c38
Petter Daae <petter.daae@gmail.com>
2020-08-15 14:20:18 +0200

    Add sql table definitions

---
+++ sql/tables.sql
@@ -0,0 +1,13 @@
+create table plant (
+    id serial primary key,
+    name varchar(255) not null
+);
+
+create table sensordata (
+    id serial primary key,
+    data int not null,
+    timestamp timestamp not null default current_timestamp,
+    type varchar(255) not null,
+    plant int not null,
+    foreign key (plant) references plant (id)
+);
