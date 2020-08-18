075f236b26ae4debd9e9ee82d62c8b894c8e4d3f
Petter Daae <petter.daae@gmail.com>
2020-08-18 20:24:37 +0200

    First working databse connection

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,54 @@
 extern crate dotenv;
 
+use http;
 use log::info;
+use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
+use postgres_openssl::MakeTlsConnector;
 use pretty_env_logger;
+use serde::{Deserialize, Serialize};
+use std::env;
+use tokio_postgres;
 use warp::Filter;
+
+#[derive(Serialize, Deserialize)]
+struct Plant {
+    name: String,
+}
+
+async fn new_plant() -> Result<impl warp::Reply, warp::Rejection> {
+    info!("New plant called");
+    let credentials = env::var("DB_CONNECTION_STRING").unwrap();
+    let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    builder.set_verify(openssl::ssl::SslVerifyMode::NONE);
+    let negotiator = MakeTlsConnector::new(builder.build());
+    //let connection = Connection::connect(credentials, TlsMode::Prefer(&negotiator)).unwrap();
+    //connection
+    //    .execute("INSERT INTO plants (name) VALUES ($1)", &[&"Papapapapa"])
+    //    .unwrap();
+
+    info!("connecting");
+    let (client, connection) = tokio_postgres::connect(&credentials, negotiator)
+        .await
+        .unwrap();
+    info!("connected|");
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Connection error: {}", e);
+        }
+    });
+
+    info!("querying");
+    client.query("SELECT * FROM plant", &[]).await.unwrap();
+
+    info!("succesfull query");
+
+    info!("New plant called");
+    Ok(warp::reply::with_status(
+        format!("Hello, World!"),
+        http::StatusCode::CREATED,
+    ))
+}
 
 #[tokio::main]
 async fn main() {
@@ -13,6 +59,8 @@ async fn main() {
     pretty_env_logger::init();
 
     info!("Talking Plants REST API Started");
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+
+    let np = warp::post().and(warp::path("plants")).and_then(new_plant);
+
+    warp::serve(np).run(([127, 0, 0, 1], 3030)).await;
 }
