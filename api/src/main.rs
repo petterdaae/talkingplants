03f19689445f8890f29834606c10eae0ffda5685
Petter Daae <petter.daae@gmail.com>
2020-08-18 16:04:48 +0200

    Add logging and config handling

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,18 @@
+extern crate dotenv;
+
+use log::info;
+use pretty_env_logger;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    // Load config into environment from .evn files
+    dotenv::dotenv().ok();
+
+    // Initialize logging
+    pretty_env_logger::init();
+
+    info!("Talking Plants REST API Started");
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
     warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
