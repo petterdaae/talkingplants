a178fe3f8d8f4de7e23f55a6d90faaccb2985be1
Petter Daae <petter.daae@gmail.com>
2020-08-18 15:52:04 +0200

    Create warp hello world

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
