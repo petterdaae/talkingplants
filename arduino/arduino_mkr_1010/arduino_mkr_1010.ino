d2eaeedeb95d7edcf468664cc1f5b3d9f79afe15
Petter Daae <petter.daae@gmail.com>
2020-08-21 14:12:19 +0200

    Finish first arduino proof of concept

--- arduino/arduino_mkr_1010/arduino_mkr_1010.ino
+++ arduino/arduino_mkr_1010/arduino_mkr_1010.ino
@@ -1,70 +1,48 @@
-/*
-  This example connects to an WPA2 Enterprise WiFi network.
-  Then it prints the  MAC address of the WiFi module,
-  the IP address obtained, and other network details.
-
-  Based on ConnectWithWPA.ino by dlf (Metodo2 srl) and Tom Igoe
-*/
 #include <SPI.h>
 #include <WiFiNINA.h>
-
 #include "arduino_secrets.h"
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = WIFI_SSID;  // your WPA2 enterprise network SSID (name)
-char user[] = WIFI_USER;  // your WPA2 enterprise username
-char pass[] = WIFI_PASS;  // your WPA2 enterprise password
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-// Initialize the Wifi client library
+char ssid[] = WIFI_SSID;
+char pass[] = WIFI_PASS;
+int status = WL_IDLE_STATUS;
 WiFiClient client;
-
-// server address:
 char server[] = "inf319.herokuapp.com";
-//IPAddress server(64,131,82,241);
-
-unsigned long lastConnectionTime = 0;            // last time you connected to the server, in milliseconds
-const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
+unsigned long lastConnectionTime = 0;
+const unsigned long postingInterval = 10L * 1000L;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ;
-  }
-
-  // attempt to connect to Wifi network:
+  while (!Serial) { ; }
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to wifi: ");
     Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
     delay(10000);
   }
-
   Serial.println("Successfully connected to wifi");
 }
 
 void loop() {
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-    if (!client.available()) {
-      Serial.println("Finished receiving http response");
-    }
-  }
-
-
-  
-  if (millis() - lastConnectionTime > postingInterval) {
-    httpRequest();
-  }
+  int moist = analogRead(A1);
+  Serial.print("Measured moist: ");
+  Serial.println(moist);
+  sendMoistureData(moist);
+  delay(10000);
 }
 
-void httpRequest() {
+void sendMoistureData(int moisture) {
+  String postData = "{\"data\": " + String(moisture) + ", \"type\":\"moisture\", \"plant\": 10}";
   client.stop();
   if (client.connectSSL(server, 443)) {
-    Serial.println("Sending http request");
-    client.println("GET /health HTTP/1.1");
+    Serial.println("Sending http request with moisture data");
+    client.println("POST /data HTTP/1.1");
     client.println("Host: inf319.herokuapp.com");
-    client.println("Connection: close");
+    client.print("Authorization: ");
+    client.println(API_KEY);
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.println(postData);
     client.println();
     lastConnectionTime = millis();
   } else {
