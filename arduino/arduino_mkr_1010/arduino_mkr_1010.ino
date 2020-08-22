aa829d775047b6a2f4476655b3e346e5209d8864
Petter Daae <petter.daae@gmail.com>
2020-08-22 15:57:21 +0200

    Refactor arduino code

--- arduino/arduino_mkr_1010/arduino_mkr_1010.ino
+++ arduino/arduino_mkr_1010/arduino_mkr_1010.ino
@@ -1,22 +1,17 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
-#include "arduino_secrets.h"
+#include "config.h"
 
-char ssid[] = WIFI_SSID;
-char pass[] = WIFI_PASS;
 int status = WL_IDLE_STATUS;
 WiFiClient client;
-char server[] = "inf319.herokuapp.com";
-unsigned long lastConnectionTime = 0;
-const unsigned long postingInterval = 10L * 1000L;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial) { ; }
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to wifi: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
+    Serial.println(WIFI_SSID);
+    status = WiFi.begin(WIFI_SSID, WIFI_PASS);
     delay(10000);
   }
   Serial.println("Successfully connected to wifi");
@@ -27,16 +22,17 @@ void loop() {
   Serial.print("Measured moist: ");
   Serial.println(moist);
   sendMoistureData(moist);
-  delay(10000);
+  delay(600000);
 }
 
 void sendMoistureData(int moisture) {
-  String postData = "{\"data\": " + String(moisture) + ", \"type\":\"moisture\", \"plant\": 10}";
+  String postData = "{\"data\": " + String(moisture) + ", \"type\":\"moisture\", \"plant\": " + String(PLANT_ID) + "}";
   client.stop();
-  if (client.connectSSL(server, 443)) {
+  if (client.connectSSL(API_HOST, 443)) {
     Serial.println("Sending http request with moisture data");
     client.println("POST /data HTTP/1.1");
-    client.println("Host: inf319.herokuapp.com");
+    client.print("Host: ");
+    client.println(API_HOST);
     client.print("Authorization: ");
     client.println(API_KEY);
     client.print("Content-Length: ");
@@ -44,8 +40,11 @@ void sendMoistureData(int moisture) {
     client.println();
     client.println(postData);
     client.println();
-    lastConnectionTime = millis();
   } else {
     Serial.println("Failed to connect to server");
   }
+}
+
+void sleep() {
+  
 }
