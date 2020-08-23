07b88a755f2553e2fcfffa1360f5ae6e37f84c35
Petter Daae <petter.daae@gmail.com>
2020-08-23 12:24:44 +0200

    Improve arduino code

--- arduino/arduino_mkr_1010/arduino_mkr_1010.ino
+++ arduino/arduino_mkr_1010/arduino_mkr_1010.ino
@@ -1,3 +1,11 @@
+/*
+ * Status codes:
+ * - 5 blinks     : Start of loop
+ * - Stable light : Measuring moist
+ * - 3 blinks     : Failed to connect to server
+ * - 2 blinks     : Starting
+ */
+
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include "config.h"
@@ -7,7 +15,11 @@ WiFiClient client;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) { ; }
+  // while (!Serial) { ; }
+  delay(3000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  blinkN(2);
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to wifi: ");
     Serial.println(WIFI_SSID);
@@ -18,7 +30,8 @@ void setup() {
 }
 
 void loop() {
-  int moist = analogRead(A1);
+  blinkN(5);
+  int moist = meassureMoist();
   Serial.print("Measured moist: ");
   Serial.println(moist);
   sendMoistureData(moist);
@@ -42,9 +55,30 @@ void sendMoistureData(int moisture) {
     client.println();
   } else {
     Serial.println("Failed to connect to server");
+    blinkN(3);
   }
 }
 
-void sleep() {
-  
+int meassureMoist() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  int samples = 10;
+  int total = 0;
+  for (int i = 0; i < samples; i++) {
+    total += analogRead(A1);
+    delay(2000);
+  }
+  int average = total / samples;
+  digitalWrite(LED_BUILTIN, LOW);
+  return average;
+}
+
+void blinkN(int n) {
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000);
+  for (int i = 0; i < n; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
 }
