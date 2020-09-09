c7cf1d6e6ee3482ff237b40b6eb1f89169efee27
Petter Daae <petter.daae@gmail.com>
2020-09-09 15:48:20 +0200

    Include keep alive stuff in main sketch

--- arduino/arduino_mkr_1010/arduino_mkr_1010.ino
+++ arduino/arduino_mkr_1010/arduino_mkr_1010.ino
@@ -1,41 +1,51 @@
-/*
- * Status codes:
- * - 5 blinks     : Start of loop
- * - Stable light : Measuring moist
- * - 3 blinks     : Failed to connect to server
- * - 2 blinks     : Starting
- */
-
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include "config.h"
 
+// Constants
+const int MOIST_IN = A1;
+const int MOIST_SWITCH = 1;
+const int KEEP_ALIVE_SWITCH = 2;
+
+// Global
 int status = WL_IDLE_STATUS;
 WiFiClient client;
 
 void setup() {
+  // Init serial connection
   Serial.begin(9600);
-  // while (!Serial) { ; }
-  delay(3000);
+  delay(2000);
+
+  // Setup output pins
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(MOIST_SWITCH, OUTPUT);
+  pinMode(KEEP_ALIVE_SWITCH, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
-  blinkN(2);
+  digitalWrite(MOIST_SWITCH, LOW);
+  digitalWrite(KEEP_ALIVE_SWITCH, LOW);
+
+  // Connect to WiFi
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to wifi: ");
     Serial.println(WIFI_SSID);
     status = WiFi.begin(WIFI_SSID, WIFI_PASS);
-    delay(10000);
   }
   Serial.println("Successfully connected to wifi");
 }
 
 void loop() {
-  blinkN(5);
+  // Measure the moist
+  keepAlive();
   int moist = meassureMoist();
   Serial.print("Measured moist: ");
   Serial.println(moist);
   sendMoistureData(moist);
-  delay(600000);
+
+  // Sleep for 60 minutes ish and keep the power bank alive
+  for (int i = 0; i < 120; i++) {
+    keepAlive();
+    delay(30000);
+  }
 }
 
 void sendMoistureData(int moisture) {
@@ -55,30 +65,24 @@ void sendMoistureData(int moisture) {
     client.println();
   } else {
     Serial.println("Failed to connect to server");
-    blinkN(3);
   }
 }
 
 int meassureMoist() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  int samples = 10;
+  digitalWrite(MOIST_SWITCH, HIGH);
+  int samples = 30;
   int total = 0;
   for (int i = 0; i < samples; i++) {
-    total += analogRead(A1);
-    delay(2000);
+    total += analogRead(MOIST_IN);
+    delay(100);
   }
   int average = total / samples;
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(MOIST_SWITCH, LOW);
   return average;
 }
 
-void blinkN(int n) {
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(2000);
-  for (int i = 0; i < n; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-  }
+void keepAlive() {
+  digitalWrite(KEEP_ALIVE_SWITCH, HIGH);
+  delay(500);
+  digitalWrite(KEEP_ALIVE_SWITCH, LOW);
 }
