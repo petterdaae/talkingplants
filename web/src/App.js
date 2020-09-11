ef99adb04bf8568bb45e05c844c39a4bf982453b
Petter Daae <petter.daae@gmail.com>
2020-09-11 17:17:47 +0200

    Start on home page

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import LineChart from './components/LineChart';
+import React from "react";
 import './App.css';
+import Home from './components/Home';
 
 function App() {
-  let [data, setData] = useState([{ id: "Moisture", data: [] }]);
-
-  useEffect(() => {
-    fetch('http://localhost:8080/data').then(resp => resp.json()).then(data => setData([{ id: "Moisture", data: data.map(elem => ({ x: elem.date, y: elem.data })) }]));
-  }, []);
-
-  const width = window.innerWidth > 1800 ? window.innerWidth / 1.7 : window.innerWidth / 1.2;
-  return (
-    <div className="wrapper">
-      <LineChart data={data} width={width} height={400} />
-    </div>
-  );
+  return <Home></Home>;
 }
 
 export default App;
---
+++ web/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return <div>
+        <h1 className="header"><span role="img" aria-label="plant emoji">🌱</span> Talking Plants</h1>
+    </div>;
+}
+
+export default Home;
