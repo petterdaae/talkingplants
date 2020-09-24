98faa0c954fdd1dd59cdc60fd2a0e2a551177717
Petter Daae <petter.daae@gmail.com>
2020-09-24 13:21:25 +0200

    Experiment with airbnb/visx brushchart

--- web/src/components/Plant/index.js
+++ web/src/components/Plant/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import RoundedImage from "../RoundedImage";
 import Data from "./Data";
 import styled from "@emotion/styled";
+import LineChart from "../LineChart";
+import BrushChart from "../BrushChart";
 
 const Wrapper = styled.div`
     display: inline-block;
@@ -15,10 +17,12 @@ function Plant({ id }) {
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/data?plant=${id}`).then(data => data.json()).then(json => setData(json));
     }, [id]);
+    const lineChartData = [{ id: 'moisture', data: data.map((entry => ({ x: entry.timestamp.substring(0, 10), y: entry.data }))) }];
     return (
         <Wrapper>
             <RoundedImage name="begonia.jpg" size="250px"></RoundedImage>
             <StyledHeader>Begonia Maculata</StyledHeader>
+            <BrushChart width={500} height={500}></BrushChart>
             <Data data={data}></Data>
         </Wrapper>
     );
