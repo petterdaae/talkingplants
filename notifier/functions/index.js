f3dd48bb7695e660a2f11b0414e5b5ce79085045
Petter Daae <petter.daae@gmail.com>
2020-09-25 13:43:58 +0200

    Refactor

--- notifier/functions/index.js
+++ notifier/functions/index.js
@@ -2,37 +2,36 @@ const dotenv = require('dotenv')
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 
-// Load configuration from dotenv files
 dotenv.config();
 
-exports.sendEmail = functions.https.onRequest((request, response) => {
-    const from = process.env.FROM_EMAIL;
-    const pass = process.env.FROM_PASS;
-    const to = process.env.TO_EMAIL;
+const LOW_BATTERY_MAIL_OPTIONS = {
+    from: `Talking Plants <${process.env.FROM_EMAIL}>`,
+    to: process.env.TO_EMAIL,
+    subject: "No more power",
+    text: "Your plant has not been talking for a while. It is probably out of power."
+};
 
+function sendEmail(options, callback) {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
         secure: false,
         requireTLS: true,
         auth: {
-            user: from,
-            pass: pass
+            user: process.env.FROM_EMAIL,
+            pass: process.env.FROM_PASS
         }
     });
+    transporter.sendMail(options, callback);
+}
 
-    const mailOptions = {
-        from: `Talking Plants <${from}>`,
-        to: to,
-        subject: "Test",
-        text: "Hello world!"
-    }
-
-    transporter.sendMail(mailOptions, (err, info) => {
+exports.sendEmail = functions.https.onRequest((_, response) => {
+    sendEmail(LOW_BATTERY_MAIL_OPTIONS, (err, _) => {
         if (err) {
             response.send(err.toString());
         } else {
             response.send('Success');
         }
+
     });
 });
