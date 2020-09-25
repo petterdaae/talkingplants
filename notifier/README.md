1a283ff050bf5b5509bcd191ceec92e70920bf1d
Petter Daae <petter.daae@gmail.com>
2020-09-25 13:35:52 +0200

    First successfull send with nodemailer

--- notifier/README.md
+++ notifier/README.md
@@ -1,1 +1,18 @@
 # Notifier
+
+## Setting up an email address to send emails from
+* *This was tricky*
+* Create a new gmail account
+* Enable two factor authentication
+* Generate an app password
+* Put your credentials in a file similar to `.env.example`
+
+## Development Setup
+- `sudo npm i -g firebase-tools`
+- `firebase login`
+- Create your own firebase project
+- TODO: Document how to create .firebaserc
+- `firebase emulators:start --only functions`
+
+## Deploying
+- `firebase deploy --only functions`
--- notifier/functions/index.js
+++ notifier/functions/index.js
@@ -1,8 +1,38 @@
+const dotenv = require('dotenv')
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+// Load configuration from dotenv files
+dotenv.config();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.sendEmail = functions.https.onRequest((request, response) => {
+    const from = process.env.FROM_EMAIL;
+    const pass = process.env.FROM_PASS;
+    const to = process.env.TO_EMAIL;
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: from,
+            pass: pass
+        }
+    });
+
+    const mailOptions = {
+        from: `Talking Plants <${from}>`,
+        to: to,
+        subject: "Test",
+        text: "Hello world!"
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            response.send(err.toString());
+        } else {
+            response.send('Success');
+        }
+    });
 });
