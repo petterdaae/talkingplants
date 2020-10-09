caa43eb453ad67d000d1105b50604e7a4f0236fb
Petter Daae <petter.daae@gmail.com>
2020-10-09 13:35:34 +0200

    Add low moisture notifier

--- notifier/functions/index.js
+++ notifier/functions/index.js
@@ -12,6 +12,13 @@ const LOW_BATTERY_MAIL_OPTIONS = {
     text: "Your plant has not been talking for a while. It is probably out of power."
 };
 
+let LOW_MOISTURE_MAIL_OPTIONS = (name) => ({
+    from: `Talking Plants <${process.env.FROM_EMAIL}>`,
+    to: process.env.TO_EMAIL,
+    subject: `${name} is thirsty!`,
+    text: `${name} needs water soon or it will die.`
+});
+
 function sendEmail(options, callback) {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
@@ -51,14 +58,61 @@ exports.outOfPowerNotfier = functions.pubsub.schedule('every 70 minutes').onRun(
         console.log(diff);
 
         const MINUTE = 60 * 1000;
-        const PADDING = 10 * MINUTE;
+        const PADDING = 3 * MINUTE;
 
         if (diff > 70 * MINUTE && diff < 2 * 70 * MINUTE + PADDING) {
             sendEmail(LOW_BATTERY_MAIL_OPTIONS, (err, _) => {
                 if (err) {
                     console.error(err.toString());
                 } else {
-                    console.log('Success');
+                    console.log('Successfully sent out of power mail.');
+                }
+            });
+        }
+    }
+    await client.end();
+    return null;
+});
+
+exports.lowMoistureNotifier = functions.pubsub.schedule("every 70 minutes").onRun(async context => {
+    const client = new pg.Client({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        ssl: true
+    });
+    await client.connect();
+
+    const plants = await client.query("SELECT name, id FROM plant")
+
+    for (let plant of plants) {
+        let id = plant.id;
+        let name = plant.name;
+
+        let response = await client.query(
+            "SELECT data FROM sensordata WHERE plant=$1 ORDER BY timestamp desc LIMIT 100", [id]
+        );
+
+        let moisture = response.rows.map(row => row.data);
+
+        const LIMIT = 900;
+
+        let count = 0;
+
+        for (let data of moisture) {
+            if (data > LIMIT) {
+                count++;
+            }
+        }
+
+        if (count === 7) {
+            sendEmail(LOW_MOISTURE_MAIL_OPTIONS(name), (err, _) => {
+                if (err) {
+                    console.error(err.toString());
+                } else {
+                    console.log('Successfully sent low moisture mail.');
                 }
             });
         }
