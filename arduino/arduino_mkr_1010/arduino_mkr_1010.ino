1d637cdeac187f31d6b53db4c87483c6c6fe405c
Petter Daae <petter.daae@gmail.com>
2020-12-22 14:22:30 +0100

    Update sketch and add new plant

--- arduino/arduino_mkr_1010/arduino_mkr_1010.ino
+++ arduino/arduino_mkr_1010/arduino_mkr_1010.ino
@@ -16,20 +16,15 @@ void setup() {
   // Init serial connection
   Serial.begin(9600);
   delay(2000);
-
   Serial.println("Starting ...");
-
   // Setup output pins
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(MOIST_SWITCH, OUTPUT);
-  pinMode(KEEP_ALIVE_SWITCH, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(MOIST_SWITCH, LOW);
-  digitalWrite(KEEP_ALIVE_SWITCH, LOW);
 }
 
 void connectToWifi() {
-  // Connect to WiFi
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to wifi: ");
     Serial.println(WIFI_SSID);
@@ -40,34 +35,16 @@ void connectToWifi() {
 
 void loop() {
   Serial.println("Going into sleep");
-  // Sleep for approximately 1 hour and keep the power bank alive
-  for (int i = 0; i < 120; i++) {
-    keepAlive();
-    delay(15000);
-  }
-
+  // Sleep for approximately 1 hour
+  delay(60 * 60 * 1000);
   Serial.println("Waking up");
-  
-  keepAlive();
+  Serial.println("Meassuring moist");
   int moist = meassureMoist();
   Serial.print("Meassured moist: ");
   Serial.println(moist);
-
-  keepAlive();
   connectToWifi();
-  
-  keepAlive();
   sendMoistureData(moist);
-
-  Serial.println("Restting ...");
   delay(1000);
-  reset();
-}
-
-void reset() {
-  digitalWrite(RESET_SWITCH, HIGH);
-  pinMode(RESET_SWITCH, OUTPUT);
-  digitalWrite(RESET_SWITCH, LOW);
 }
 
 void sendMoistureData(int moisture) {
@@ -92,23 +69,15 @@ void sendMoistureData(int moisture) {
 
 int meassureMoist() {
   digitalWrite(MOIST_SWITCH, HIGH);
-  // Wait for sensor to stabalize??
   delay(5000);
   int samples = 300;
   int total = 0;
   for (int i = 0; i < samples; i++) {
     int current = analogRead(MOIST_IN);
     total += current;
-    Serial.println(current);
     delay(50);
   }
   int average = total / samples;
   digitalWrite(MOIST_SWITCH, LOW);
   return average;
-}
-
-void keepAlive() {
-  digitalWrite(KEEP_ALIVE_SWITCH, HIGH);
-  delay(2000);
-  digitalWrite(KEEP_ALIVE_SWITCH, LOW);
 }
